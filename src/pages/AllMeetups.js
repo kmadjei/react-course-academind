8312de7887498f3ac8c16bd193f9230a024ad710
Kenneth Adjei <[kmadjei25@gmail.com]>
2021-08-08 19:09:12 -0400

    getting started with fetch data and useEffect

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_DATA = [
@@ -22,12 +24,50 @@ const DUMMY_DATA = [
   ];
   
   function AllMeetupsPage() {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    // useEffect hook ??
+    useEffect(() => {
+      setIsLoading(true)
+      fetch(
+        'https://react-course-academind-63f90-default-rtdb.firebaseio.com/meetups.json'
+      )
+      .then((response) => {
+          return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key] // "..." --> JS spread operator
+          }
+
+          meetups.push(meetup)
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+    }, [])
+
+    // Renders to wait for a successful HTTP response
+    if (isLoading) {
+      return (
+        <section>
+          <p>Loading...</p>
+        </section>        
+      )
+    }
+
     return (
       <section>
         <h1>All Meetups</h1>
         <ul>
             {/* Outputting array list */}
-            <MeetupList meetups={DUMMY_DATA} />
+            <MeetupList meetups={loadedMeetups /*DUMMY_DATA*/} />
         </ul>
       </section>
     );
